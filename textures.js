2290ea29b93f2b9d94e5031f8e64a44c54db43e4
Darkmatraque <kevin2007baumann@gmail.com>
2026-03-27 16:24:21 +0100

    Update textures.js

--- textures.js
+++ textures.js
@@ -1,359 +1,329 @@
-/* ============================
-   PALETTE GLOBALE
-   ============================ */
-
-const TEX_COLORS = {
-  // verts
-  G1: "#7ecf4f",
-  G2: "#6ab842",
-  G3: "#4f8f2f",
-
-  // terre
-  D1: "#6b4a2f",
-  D2: "#4f341f",
-
-  // pierre
-  S1: "#8a8a8a",
-  S2: "#6f6f6f",
-  S3: "#5a5a5a",
-
-  // sable / gravier
-  SA1: "#e7d39a",
-  SA2: "#d1c08a",
-  GR1: "#9a9a9a",
-  GR2: "#7f7f7f",
-
-  // bois
-  W1: "#8b5a2b",
-  W2: "#5a3a1a",
-  P1: "#c89a5a",
-  P2: "#a87a3a",
-
-  // feuilles
-  L1: "#5fa83a",
-  L2: "#3f7a24",
-
-  // verre
-  GL1: "#b0e0ff",
-  GL2: "#80c0ff",
-
-  // minerais
-  C1: "#2b2b2b",   // charbon
-  IR1: "#d8c8a8",  // fer
-  CU1: "#c47c3c",  // cuivre
-  AU1: "#f2d14a",  // or
-  DI1: "#4fe3e8",  // diamant
-
-  // lumière
-  T1: "#ffdd66",   // torche
-  T2: "#ffbb33",
-  GLO1: "#ffcc55",
-  GLO2: "#e0aa33",
-
-  // eau / lave
-  WA1: "#3a6fd8",
-  WA2: "#2a4fa8",
-  LA1: "#ff5a1a",
-  LA2: "#d83a0f",
-
-  // coffre / porte
-  CH1: "#8b5a2b",
-  CH2: "#5a3a1a",
-  CH3: "#c89a5a",
-  DO1: "#b87a3a",
-  DO2: "#8a5a2a"
-};
-
-/* ============================
-   PATTERNS ASCII 8x8
-   ============================ */
+// ============================
+//  TEXTURES ASCII
+// ============================
 
 const BLOCK_PATTERNS = {
-  [BLOCK.GRASS]: [
-    "G1G1G1G1G1G1G1G1",
-    "G1G1G2G1G2G1G1G1",
-    "G2G1G2G1G2G2G1G1",
-    "G2G2G1G2G1G2G2G1",
-    "D1D1D1D1D1D1D1D1",
-    "D1D2D1D2D1D2D1D2",
-    "D2D1D2D1D2D1D2D1",
-    "D1D1D1D1D1D1D1D1"
+  grass: [
+    "...............,,",
+    ".............,###",
+    "..........,######",
+    "....,###########.",
+    "....###########..",
+    "....##########...",
+    "....#########....",
+    "....########.....",
+    "....########.....",
+    "....########.....",
+    "....########.....",
+    "....########.....",
+    "....########.....",
+    "....########.....",
+    "....########.....",
+    "....########....."
   ],
-
-  [BLOCK.DIRT]: [
-    "D1D1D1D1D1D1D1D1",
-    "D1D2D1D1D2D1D2D1",
-    "D1D1D1D2D1D1D1D2",
-    "D2D1D1D1D2D1D1D1",
-    "D1D1D2D1D1D2D1D1",
-    "D1D2D1D1D2D1D1D2",
-    "D1D1D1D2D1D1D1D1",
-    "D1D1D1D1D1D1D1D1"
+  dirt: [
+    "################",
+    "################",
+    "################",
+    "################",
+    "################",
+    "################",
+    "################",
+    "################",
+    "################",
+    "################",
+    "################",
+    "################",
+    "################",
+    "################",
+    "################",
+    "################"
   ],
-
-  [BLOCK.STONE]: [
-    "S1S1S1S1S1S1S1S1",
-    "S1S2S1S1S2S1S1S1",
-    "S1S1S3S1S1S3S1S1",
-    "S1S1S1S2S1S1S2S1",
-    "S2S1S1S1S2S1S1S1",
-    "S1S3S1S1S1S3S1S1",
-    "S1S1S1S2S1S1S1S2",
-    "S1S1S1S1S1S1S1S1"
+  stone: [
+    "################",
+    "##..####..######",
+    "######..########",
+    "##########..####",
+    "####..##########",
+    "########..######",
+    "######..########",
+    "################",
+    "########..######",
+    "######..########",
+    "####..##########",
+    "##########..####",
+    "######..########",
+    "################",
+    "################",
+    "################"
   ],
-
-  [BLOCK.COBBLESTONE]: [
-    "S3S3S2S3S3S2S3S3",
-    "S2S3S3S2S3S3S2S3",
-    "S3S2S3S3S2S3S3S2",
-    "S3S3S2S3S3S2S3S3",
-    "S2S3S3S2S3S3S2S3",
-    "S3S2S3S3S2S3S3S2",
-    "S3S3S2S3S3S2S3S3",
-    "S2S3S3S2S3S3S2S3"
+  sand: [
+    "................",
+    "................",
+    "....::::::::....",
+    "....::::::::....",
+    "................",
+    "................",
+    "....::::::::....",
+    "....::::::::....",
+    "................",
+    "................",
+    "....::::::::....",
+    "....::::::::....",
+    "................",
+    "................",
+    "................",
+    "................"
   ],
-
-  [BLOCK.SAND]: [
-    "SA1SA1SA1SA1SA1SA1SA1SA1",
-    "SA1SA2SA1SA1SA2SA1SA1SA2",
-    "SA1SA1SA1SA2SA1SA1SA2SA1",
-    "SA2SA1SA1SA1SA2SA1SA1SA1",
-    "SA1SA1SA2SA1SA1SA2SA1SA1",
-    "SA1SA2SA1SA1SA2SA1SA1SA2",
-    "SA1SA1SA1SA2SA1SA1SA2SA1",
-    "SA1SA1SA1SA1SA1SA1SA1SA1"
+  snow: [
+    "****************",
+    "****************",
+    "****************",
+    "****************",
+    "****************",
+    "****************",
+    "****************",
+    "****************",
+    "****************",
+    "****************",
+    "****************",
+    "****************",
+    "****************",
+    "****************",
+    "****************",
+    "****************"
   ],
-
-  [BLOCK.GRAVEL]: [
-    "GR1GR1GR1GR1GR1GR1GR1GR1",
-    "GR1GR2GR1GR1GR2GR1GR1GR2",
-    "GR1GR1GR2GR1GR1GR2GR1GR1",
-    "GR2GR1GR1GR2GR1GR1GR2GR1",
-    "GR1GR1GR2GR1GR1GR2GR1GR1",
-    "GR1GR2GR1GR1GR2GR1GR1GR2",
-    "GR1GR1GR1GR2GR1GR1GR2GR1",
-    "GR1GR1GR1GR1GR1GR1GR1GR1"
+  water: [
+    "~~~~~~~~~~~~~~~~",
+    "~~~~~~~~~~~~~~~~",
+    "~~~~~~~~~~~~~~~~",
+    "~~~~~~~~~~~~~~~~",
+    "~~~~~~~~~~~~~~~~",
+    "~~~~~~~~~~~~~~~~",
+    "~~~~~~~~~~~~~~~~",
+    "~~~~~~~~~~~~~~~~",
+    "~~~~~~~~~~~~~~~~",
+    "~~~~~~~~~~~~~~~~",
+    "~~~~~~~~~~~~~~~~",
+    "~~~~~~~~~~~~~~~~",
+    "~~~~~~~~~~~~~~~~",
+    "~~~~~~~~~~~~~~~~",
+    "~~~~~~~~~~~~~~~~",
+    "~~~~~~~~~~~~~~~~"
   ],
-
-  [BLOCK.LOG]: [
-    "W2W2W2W2W2W2W2W2",
-    "W2W1W1W1W1W1W1W2",
-    "W2W1W2W1W2W1W1W2",
-    "W2W1W1W1W1W1W1W2",
-    "W2W1W2W1W2W1W1W2",
-    "W2W1W1W1W1W1W1W2",
-    "W2W1W1W2W1W1W1W2",
-    "W2W2W2W2W2W2W2W2"
+  wood: [
+    "||||||||||||||||",
+    "||||||||||||||||",
+    "||||||||||||||||",
+    "||||||||||||||||",
+    "||||||||||||||||",
+    "||||||||||||||||",
+    "||||||||||||||||",
+    "||||||||||||||||",
+    "||||||||||||||||",
+    "||||||||||||||||",
+    "||||||||||||||||",
+    "||||||||||||||||",
+    "||||||||||||||||",
+    "||||||||||||||||",
+    "||||||||||||||||",
+    "||||||||||||||||"
   ],
-
-  [BLOCK.PLANKS]: [
-    "P1P1P1P1P1P1P1P1",
-    "P1P2P1P1P2P1P1P2",
-    "P1P1P1P2P1P1P2P1",
-    "P2P1P1P1P2P1P1P1",
-    "P1P1P2P1P1P2P1P1",
-    "P1P2P1P1P2P1P1P2",
-    "P1P1P1P2P1P1P2P1",
-    "P1P1P1P1P1P1P1P1"
+  leaves: [
+    "&&&&&&&&&&&&&&&&",
+    "&&&&&&&&&&&&&&&&",
+    "&&&&&&&&&&&&&&&&",
+    "&&&&&&&&&&&&&&&&",
+    "&&&&&&&&&&&&&&&&",
+    "&&&&&&&&&&&&&&&&",
+    "&&&&&&&&&&&&&&&&",
+    "&&&&&&&&&&&&&&&&",
+    "&&&&&&&&&&&&&&&&",
+    "&&&&&&&&&&&&&&&&",
+    "&&&&&&&&&&&&&&&&",
+    "&&&&&&&&&&&&&&&&",
+    "&&&&&&&&&&&&&&&&",
+    "&&&&&&&&&&&&&&&&",
+    "&&&&&&&&&&&&&&&&",
+    "&&&&&&&&&&&&&&&&"
   ],
-
-  [BLOCK.LEAVES]: [
-    "L1L1L1L1L1L1L1L1",
-    "L1L2L1L1L2L1L1L2",
-    "L1L1L2L1L1L2L1L1",
-    "L2L1L1L2L1L1L2L1",
-    "L1L1L2L1L1L2L1L1",
-    "L1L2L1L1L2L1L1L2",
-    "L1L1L1L2L1L1L2L1",
-    "L1L1L1L1L1L1L1L1"
+  planks: [
+    "================",
+    "================",
+    "================",
+    "================",
+    "================",
+    "================",
+    "================",
+    "================",
+    "================",
+    "================",
+    "================",
+    "================",
+    "================",
+    "================",
+    "================",
+    "================"
   ],
-
-  [BLOCK.GLASS]: [
-    "GL1GL1GL1GL1GL1GL1GL1GL1",
-    "GL1GL2GL1GL1GL2GL1GL1GL2",
-    "GL1GL1GL1GL2GL1GL1GL2GL1",
-    "GL2GL1GL1GL1GL2GL1GL1GL1",
-    "GL1GL1GL2GL1GL1GL2GL1GL1",
-    "GL1GL2GL1GL1GL2GL1GL1GL2",
-    "GL1GL1GL1GL2GL1GL1GL2GL1",
-    "GL1GL1GL1GL1GL1GL1GL1GL1"
+  glass: [
+    "................",
+    ".//////////////.",
+    ".//////////////.",
+    ".//////////////.",
+    ".//////////////.",
+    ".//////////////.",
+    ".//////////////.",
+    ".//////////////.",
+    ".//////////////.",
+    ".//////////////.",
+    ".//////////////.",
+    ".//////////////.",
+    ".//////////////.",
+    ".//////////////.",
+    "................",
+    "................"
   ],
-
-  [BLOCK.TORCH]: [
-    "000000T100000000",
-    "000000T200000000",
-    "000000T200000000",
-    "000000T200000000",
-    "0000W1W1W1000000",
-    "0000W1W1W1000000",
-    "000000W100000000",
-    "0000000000000000"
+  coal_ore: [
+    "################",
+    "######..########",
+    "####..##########",
+    "########..######",
+    "######..########",
+    "##########..####",
+    "####..##########",
+    "################",
+    "########..######",
+    "######..########",
+    "####..##########",
+    "##########..####",
+    "######..########",
+    "################",
+    "################",
+    "################"
   ],
-
-  [BLOCK.CHEST]: [
-    "CH3CH3CH3CH3CH3CH3CH3CH3",
-    "CH3CH1CH1CH1CH1CH1CH1CH3",
-    "CH3CH1CH2CH2CH2CH1CH1CH3",
-    "CH3CH1CH1CH1CH1CH1CH1CH3",
-    "CH3CH2CH2CH2CH2CH2CH2CH3",
-    "CH3CH1CH1CH1CH1CH1CH1CH3",
-    "CH3CH1CH1CH1CH1CH1CH1CH3",
-    "CH3CH3CH3CH3CH3CH3CH3CH3"
+  iron_ore: [
+    "################",
+    "######::########",
+    "####::##########",
+    "########::######",
+    "######::########",
+    "##########::####",
+    "####::##########",
+    "################",
+    "########::######",
+    "######::########",
+    "####::##########",
+    "##########::####",
+    "######::########",
+    "################",
+    "################",
+    "################"
   ],
-
-  [BLOCK.DOOR_BOTTOM]: [
-    "DO1DO1DO1DO1DO1DO1DO1DO1",
-    "DO1DO2DO1DO2DO1DO2DO1DO1",
-    "DO1DO1DO1DO1DO1DO1DO1DO1",
-    "DO1DO2DO1DO2DO1DO2DO1DO1",
-    "DO1DO1DO1DO1DO1DO1DO1DO1",
-    "DO1DO2DO1DO2DO1DO2DO1DO1",
-    "DO1DO1DO1DO1DO1DO1DO1DO1",
-    "DO1DO1DO1DO1DO1DO1DO1DO1"
+  copper_ore: [
+    "################",
+    "######;;########",
+    "####;;##########",
+    "########;;######",
+    "######;;########",
+    "##########;;####",
+    "####;;##########",
+    "################",
+    "########;;######",
+    "######;;########",
+    "####;;##########",
+    "##########;;####",
+    "######;;########",
+    "################",
+    "################",
+    "################"
   ],
-
-  [BLOCK.DOOR_TOP]: [
-    "DO1DO1DO1DO1DO1DO1DO1DO1",
-    "DO1DO1DO1DO1DO1DO1DO1DO1",
-    "DO1DO2DO1DO2DO1DO2DO1DO1",
-    "DO1DO1DO1DO1DO1DO1DO1DO1",
-    "DO1DO2DO1DO2DO1DO2DO1DO1",
-    "DO1DO1DO1DO1DO1DO1DO1DO1",
-    "DO1DO1DO1DO1DO1DO1DO1DO1",
-    "DO1DO1DO1DO1DO1DO1DO1DO1"
+  gold_ore: [
+    "################",
+    "######$$########",
+    "####$$##########",
+    "########$$######",
+    "######$$########",
+    "##########$$####",
+    "####$$##########",
+    "################",
+    "########$$######",
+    "######$$########",
+    "####$$##########",
+    "##########$$####",
+    "######$$########",
+    "################",
+    "################",
+    "################"
   ],
-
-  [BLOCK.WATER]: [
-    "WA1WA1WA1WA1WA1WA1WA1WA1",
-    "WA1WA2WA1WA1WA2WA1WA1WA2",
-    "WA1WA1WA2WA1WA1WA2WA1WA1",
-    "WA2WA1WA1WA2WA1WA1WA2WA1",
-    "WA1WA1WA2WA1WA1WA2WA1WA1",
-    "WA1WA2WA1WA1WA2WA1WA1WA2",
-    "WA1WA1WA1WA2WA1WA1WA2WA1",
-    "WA1WA1WA1WA1WA1WA1WA1WA1"
-  ],
-
-  [BLOCK.LAVA]: [
-    "LA1LA1LA1LA1LA1LA1LA1LA1",
-    "LA1LA2LA1LA1LA2LA1LA1LA2",
-    "LA1LA1LA2LA1LA1LA2LA1LA1",
-    "LA2LA1LA1LA2LA1LA1LA2LA1",
-    "LA1LA1LA2LA1LA1LA2LA1LA1",
-    "LA1LA2LA1LA1LA2LA1LA1LA2",
-    "LA1LA1LA1LA2LA1LA1LA2LA1",
-    "LA1LA1LA1LA1LA1LA1LA1LA1"
-  ],
-
-  [BLOCK.COAL_ORE]: [
-    "S1S1S1S1S1S1S1S1",
-    "S1C1S1S1C1S1S1S1",
-    "S1S1S1C1S1S1C1S1",
-    "S1S1S1S1S1S1S1S1",
-    "S1C1S1S1S1C1S1S1",
-    "S1S1S1S1S1S1S1S1",
-    "S1S1C1S1S1S1C1S1",
-    "S1S1S1S1S1S1S1S1"
-  ],
-
-  [BLOCK.IRON_ORE]: [
-    "S1S1S1S1S1S1S1S1",
-    "S1IR1S1S1IR1S1S1S1",
-    "S1S1S1IR1S1S1IR1S1",
-    "S1S1S1S1S1S1S1S1",
-    "S1IR1S1S1S1IR1S1S1",
-    "S1S1S1S1S1S1S1S1",
-    "S1S1IR1S1S1S1IR1S1",
-    "S1S1S1S1S1S1S1S1"
-  ],
-
-  [BLOCK.COPPER_ORE]: [
-    "S1S1S1S1S1S1S1S1",
-    "S1CU1S1S1CU1S1S1S1",
-    "S1S1S1CU1S1S1CU1S1",
-    "S1S1S1S1S1S1S1S1",
-    "S1CU1S1S1S1CU1S1S1",
-    "S1S1S1S1S1S1S1S1",
-    "S1S1CU1S1S1S1CU1S1",
-    "S1S1S1S1S1S1S1S1"
-  ],
-
-  [BLOCK.GOLD_ORE]: [
-    "S1S1S1S1S1S1S1S1",
-    "S1AU1S1S1AU1S1S1S1",
-    "S1S1S1AU1S1S1AU1S1",
-    "S1S1S1S1S1S1S1S1",
-    "S1AU1S1S1S1AU1S1S1",
-    "S1S1S1S1S1S1S1S1",
-    "S1S1AU1S1S1S1AU1S1",
-    "S1S1S1S1S1S1S1S1"
-  ],
-
-  [BLOCK.DIAMOND_ORE]: [
-    "S1S1S1S1S1S1S1S1",
-    "S1DI1S1S1DI1S1S1S1",
-    "S1S1S1DI1S1S1DI1S1",
-    "S1S1S1S1S1S1S1S1",
-    "S1DI1S1S1S1DI1S1S1",
-    "S1S1S1S1S1S1S1S1",
-    "S1S1DI1S1S1S1DI1S1",
-    "S1S1S1S1S1S1S1S1"
-  ],
-
-  [BLOCK.GLOWSTONE]: [
-    "GLO1GLO1GLO1GLO1GLO1GLO1GLO1GLO1",
-    "GLO1GLO2GLO1GLO1GLO2GLO1GLO1GLO2",
-    "GLO1GLO1GLO2GLO1GLO1GLO2GLO1GLO1",
-    "GLO2GLO1GLO1GLO2GLO1GLO1GLO2GLO1",
-    "GLO1GLO1GLO2GLO1GLO1GLO2GLO1GLO1",
-    "GLO1GLO2GLO1GLO1GLO2GLO1GLO1GLO2",
-    "GLO1GLO1GLO1GLO2GLO1GLO1GLO2GLO1",
-    "GLO1GLO1GLO1GLO1GLO1GLO1GLO1GLO1"
+  diamond_ore: [
+    "################",
+    "######@@########",
+    "####@@##########",
+    "########@@######",
+    "######@@########",
+    "##########@@####",
+    "####@@##########",
+    "################",
+    "########@@######",
+    "######@@########",
+    "####@@##########",
+    "##########@@####",
+    "######@@########",
+    "################",
+    "################",
+    "################"
   ]
 };
 
-/* ============================
-   GENERATEUR DE TEXTURE
-   ============================ */
+function charToColor(ch) {
+  switch (ch) {
+    case "#": return "#7f7f7f"; // stone
+    case ".": return "#8b5a2b"; // dirt
+    case ",": return "#2ecc71"; // grass
+    case ":": return "#f4d03f"; // sand
+    case "*": return "#ecf0f1"; // snow
+    case "~": return "#3498db"; // water
+    case "|": return "#8e5b3c"; // wood
+    case "&": return "#27ae60"; // leaves
+    case "=": return "#d0a06f"; // planks
+    case "/": return "rgba(255,255,255,0.4)"; // glass
+    case "$": return "#f1c40f"; // gold
+    case "@": return "#1abc9c"; // diamond
+    case ";": return "#e67e22"; // copper
+    case ":": return "#bdc3c7"; // iron (reuse)
+    default: return "magenta"; // debug
+  }
+}
 
-const textureCache = new Map();
-
-function generateBlockTextureCanvas(blockId, size = 16) {
-  if (textureCache.has(blockId)) return textureCache.get(blockId);
-
-  const pattern = BLOCK_PATTERNS[blockId];
-  if (!pattern) return null;
+function generateBlockTextureCanvas(blockId, size) {
+  const def = BLOCK_DEFS[blockId];
+  const patternName = def && def.texture ? def.texture : null;
+  const pattern = patternName && BLOCK_PATTERNS[patternName];
 
   const canvas = document.createElement("canvas");
-  canvas.width = canvas.height = size;
+  canvas.width = size;
+  canvas.height = size;
   const ctx = canvas.getContext("2d");
-  const img = ctx.createImageData(size, size);
 
-  const h = pattern.length;
-  const w = pattern[0].length / 2; // chaque code = 2 chars (ex: "G1")
+  if (!pattern) {
+    ctx.fillStyle = "magenta";
+    ctx.fillRect(0, 0, size, size);
+    return canvas;
+  }
 
-  for (let y = 0; y < size; y++) {
-    for (let x = 0; x < size; x++) {
-      const px = Math.floor(x * w / size);
-      const py = Math.floor(y * h / size);
+  const rows = pattern.length;
+  const cols = pattern[0].length;
+  const pxW = size / cols;
+  const pxH = size / rows;
 
-      const row = pattern[py];
-      const code = row.slice(px * 2, px * 2 + 2); // ex "G1"
-      const color = TEX_COLORS[code] || "#ff00ff";
-
-      const r = parseInt(color.slice(1, 3), 16);
-      const g = parseInt(color.slice(3, 5), 16);
-      const b = parseInt(color.slice(5, 7), 16);
-
-      const idx = (x + y * size) * 4;
-      img.data[idx + 0] = r;
-      img.data[idx + 1] = g;
-      img.data[idx + 2] = b;
-      img.data[idx + 3] = 255;
+  for (let y = 0; y < rows; y++) {
+    const row = pattern[y];
+    for (let x = 0; x < cols; x++) {
+      const ch = row[x];
+      ctx.fillStyle = charToColor(ch);
+      ctx.fillRect(x * pxW, y * pxH, pxW, pxH);
     }
   }
 
-  ctx.putImageData(img, 0, 0);
-  textureCache.set(blockId, canvas);
   return canvas;
 }
