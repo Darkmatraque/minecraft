a9d51d0a62058b1c3a462ba953e1d04172d92280
Darkmatraque <kevin2007baumann@gmail.com>
2026-03-27 15:28:37 +0100

    Update blocks.js

--- blocks.js
+++ blocks.js
@@ -4,20 +4,34 @@ const BLOCK = {
   DIRT: 2,
   STONE: 3,
   SAND: 4,
-  WOOD: 5,
-  LEAVES: 6,
+  GRAVEL: 5,
+  LOG: 6,
   PLANKS: 7,
-  GLASS: 8
+  LEAVES: 8,
+  GLASS: 9,
+
+  COAL_ORE: 10,
+  IRON_ORE: 11,
+  COPPER_ORE: 12,
+  GOLD_ORE: 13,
+  DIAMOND_ORE: 14
 };
 
 const BLOCK_DEFS = {
-  [BLOCK.AIR]:   { id: BLOCK.AIR,   name: "Air",    color: 0x000000, solid: false },
-  [BLOCK.GRASS]: { id: BLOCK.GRASS, name: "Herbe",  color: 0x3ba53b, solid: true  },
-  [BLOCK.DIRT]:  { id: BLOCK.DIRT,  name: "Terre",  color: 0x8b5a2b, solid: true  },
-  [BLOCK.STONE]: { id: BLOCK.STONE, name: "Pierre", color: 0x777777, solid: true  },
-  [BLOCK.SAND]:  { id: BLOCK.SAND,  name: "Sable",  color: 0xd9d29a, solid: true  },
-  [BLOCK.WOOD]:  { id: BLOCK.WOOD,  name: "Bois",   color: 0x8b5a2b, solid: true  },
-  [BLOCK.LEAVES]:{ id: BLOCK.LEAVES,name: "Feuilles",color: 0x2f8f2f, solid: true },
-  [BLOCK.PLANKS]:{ id: BLOCK.PLANKS,name: "Planches",color: 0xc8a46b, solid: true },
-  [BLOCK.GLASS]: { id: BLOCK.GLASS, name: "Verre",  color: 0x99c9ff, solid: false }
+  [BLOCK.AIR]:   { solid: false },
+  [BLOCK.GRASS]: { solid: true },
+  [BLOCK.DIRT]:  { solid: true },
+  [BLOCK.STONE]: { solid: true },
+  [BLOCK.SAND]:  { solid: true },
+  [BLOCK.GRAVEL]:{ solid: true },
+  [BLOCK.LOG]:   { solid: true },
+  [BLOCK.PLANKS]:{ solid: true },
+  [BLOCK.LEAVES]:{ solid: true },
+  [BLOCK.GLASS]: { solid: true },
+
+  [BLOCK.COAL_ORE]:    { solid: true },
+  [BLOCK.IRON_ORE]:    { solid: true },
+  [BLOCK.COPPER_ORE]:  { solid: true },
+  [BLOCK.GOLD_ORE]:    { solid: true },
+  [BLOCK.DIAMOND_ORE]: { solid: true }
 };
